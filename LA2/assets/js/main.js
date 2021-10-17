a0348b2f187e8bfad53e273b38d0c8c402602582
Devang <risingentropy20@gmail.com>
2021-10-17 23:11:03 +0530

    wow

--- LA2/assets/js/main.js
+++ LA2/assets/js/main.js
@@ -1,1 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {}, !1);
+
+$('#contactForm').submit(function( event ) {
+    event.preventDefault();
+    alert( "Form submitted successfully!" );
+  })
